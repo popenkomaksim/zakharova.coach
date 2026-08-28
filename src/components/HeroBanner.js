b17272684b551e66fcf4863d3d9a7518afbc5b9f
Maksym Popenko <3439938+popenkomaksim@users.noreply.github.com>
2026-08-28 17:25:18 +0300

    Working on hero

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,12 +1,14 @@
-import "@fontsource/jost/900.css";
+import "@fontsource/montserrat/900.css";
 import styled from "styled-components";
 import { Image, Typography } from "antd";
 import frameSvg from "./frame.svg";
 
 const StyledComposition = styled.div`
+  font-family: "Montserrat", sans-serif;
+  font-weight: 900;
   position: relative;
   width: 100%;
-  padding-top: 74.5%;
+  padding-top: 49.5%;
   margin: 1em 0 4em 0;
   @media (max-width: 768px) {
     padding-top: 130%;
@@ -15,35 +17,34 @@ const StyledComposition = styled.div`
 
 const StyledFrame = styled.div`
   position: absolute;
-  // left: 5.6%;
-  top: 4%;
-  width: 80%;
-  height: 50%;
-  // margin: 0 10em;
-  // padding: 2em;
+  inset: 0;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  padding: 3%;
   background-image: url("${frameSvg}");
   background-repeat: no-repeat;
+  background-origin: content-box;
   background-size: 100% 100%;
 `;
 
-const StyledTagBox = styled.div`'
-  font-family: 'Jost', sans-serif;
+const StyledTagBox = styled.div`
   position: absolute;
   left: 9.6%;
   top: 8.9%;
   width: 40%;
-  z-index: 3;
+  z-index: 2;
   padding: 0 0.5em 0.5em 0;
-  font-size: clamp(0.85em, 2.2vw, 1.05em);
-  font-weight: 900;
+  font-size: clamp(0.85em, 2.2vw, 4.05em);
   line-height: 1.3;
 `;
 
 const StyledMountainPhotoWrapper = styled.div`
   position: absolute;
-  top: 0;
+  top: -2%;
   right: 5%;
-  width: 29%;
+  width: 48%;
+  z-index: 1;
 `;
 
 const StyledRunnerPhotoWrapper = styled.div`
@@ -51,15 +52,20 @@ const StyledRunnerPhotoWrapper = styled.div`
   top: 30.5%;
   left: 13%;
   width: 43.5%;
+  max-width: 39em;
+  z-index: 1;
 `;
 
 const StyledPlanText = styled(Typography.Title)`
   &&& {
+    font-family: "Montserrat", sans-serif;
+    font-weight: 900;
     position: absolute;
-    top: 44%;
+    top: 47%;
     right: 20%;
     width: 46%;
     margin: 0;
+    z-index: 2;
     text-align: right;
     line-height: 1.15;
     text-underline-offset: 0.1em;
@@ -69,10 +75,13 @@ const StyledPlanText = styled(Typography.Title)`
 
 const StyledBodyText = styled(Typography.Title)`
   &&& {
+    font-family: "Montserrat", sans-serif;
+    font-weight: 900;
     position: absolute;
-    top: 77%;
+    top: 70%;
     right: 4.5%;
     margin: 0;
+    z-index: 2;
     line-height: 2.05;
     white-space: nowrap;
     font-size: clamp(1.8em, 8vw, 3.4em);
@@ -81,38 +90,44 @@ const StyledBodyText = styled(Typography.Title)`
 
 const StyledRulesText = styled(Typography.Title)`
   &&& {
+    font-family: "Montserrat", sans-serif;
+    font-weight: 900;
     position: absolute;
     top: 84.5%;
     right: 4%;
     margin: 0;
+    z-index: 2;
     line-height: 1.05;
     white-space: nowrap;
     font-size: clamp(1.8em, 8vw, 3.4em);
   }
 `;
 
-const HeroBanner = () => (
-  <StyledComposition>
-    <StyledFrame />
-    <StyledTagBox>Українська ідентичність через спорт</StyledTagBox>
-    <StyledMountainPhotoWrapper>
-      <Image
-        src="./photo_2025-10-23 18.36.45.png"
-        preview={false}
-        style={{ filter: "grayscale(1)", display: "block", width: "100%" }}
-      />
-    </StyledMountainPhotoWrapper>
-    <StyledRunnerPhotoWrapper>
-      <Image
-        src="./PolinaJump.png"
-        preview={false}
-        style={{ filter: "grayscale(1)", display: "block", width: "100%" }}
-      />
-    </StyledRunnerPhotoWrapper>
-    <StyledPlanText level={2}>Мій тренувальний план</StyledPlanText>
-    <StyledBodyText level={1}>Твоє тіло</StyledBodyText>
-    <StyledRulesText level={1}>Мої правила</StyledRulesText>
-  </StyledComposition>
-);
+const HeroBanner = () => {
+  return (
+    <StyledComposition>
+      <StyledFrame>
+        <StyledTagBox>Українська ідентичність через спорт</StyledTagBox>
+        <StyledPlanText level={2}>Наш тренувальний план</StyledPlanText>
+        <StyledBodyText level={1}>Твоє тіло</StyledBodyText>
+        <StyledRulesText level={1}>Мої правила</StyledRulesText>
+      </StyledFrame>
+      <StyledMountainPhotoWrapper>
+        <Image
+          src="./design_kazbek.jpeg"
+          preview={false}
+          style={{ display: "block", width: "100%" }}
+        />
+      </StyledMountainPhotoWrapper>
+      <StyledRunnerPhotoWrapper>
+        <Image
+          src="./PolinaJump.jpeg"
+          preview={false}
+          style={{ display: "block", width: "100%" }}
+        />
+      </StyledRunnerPhotoWrapper>
+    </StyledComposition>
+  );
+};
 
 export default HeroBanner;
