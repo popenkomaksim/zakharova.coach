10bfed9a8a12cab9347197163ca975e75c2e2586
Maksym Popenko <3439938+popenkomaksim@users.noreply.github.com>
2026-09-14 09:46:17 +0300

    Remove telegram button from floating actions

--- src/components/FloatingActions.js
+++ src/components/FloatingActions.js
@@ -1,36 +1,17 @@
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { FloatButton } from "antd";
-import { FaTelegram } from "react-icons/fa";
 import { IoMenuOutline } from "react-icons/io5";
-
-const StyledTooltipTelegram = styled(FaTelegram)`
-  font-size: 1em;
-`;
 
 const StyledMenuIcon = styled(IoMenuOutline)`
   font-size: 1em;
 `;
 
-const StyledTelegramFloatButton = styled(FloatButton)`
-  &&& {
-    right: 94px;
-  }
-`;
-
-const FloatingActions = ({ redirectToTelegram, onMenuClick }) => (
-  <>
-    <StyledTelegramFloatButton
-      onClick={redirectToTelegram}
-      badge={{ count: 1 }}
-      icon={<StyledTooltipTelegram />}
-    />
-    <FloatButton onClick={onMenuClick} icon={<StyledMenuIcon />} />
-  </>
+const FloatingActions = ({ onMenuClick }) => (
+  <FloatButton onClick={onMenuClick} icon={<StyledMenuIcon />} />
 );
 
 FloatingActions.propTypes = {
-  redirectToTelegram: PropTypes.func.isRequired,
   onMenuClick: PropTypes.func.isRequired,
 };
 
