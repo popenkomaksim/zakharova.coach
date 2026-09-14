a02a16b340cd072c6c93859600629df231450947
Maksym Popenko <3439938+popenkomaksim@users.noreply.github.com>
2026-09-14 09:46:35 +0300

    Remove telegram button from floating actions

--- src/components/PageShell.js
+++ src/components/PageShell.js
@@ -4,19 +4,14 @@ import Header from "./Header";
 import NavModal from "./NavModal";
 import FloatingActions from "./FloatingActions";
 import Footer from "./Footer";
-import useContactRedirects from "../hooks/useContactRedirects";
 
 const PageShell = ({ children }) => {
   const [navOpen, setNavOpen] = useState(false);
-  const { redirectToTelegram } = useContactRedirects();
 
   return (
     <>
       <NavModal open={navOpen} onClose={() => setNavOpen(false)} />
-      <FloatingActions
-        redirectToTelegram={redirectToTelegram}
-        onMenuClick={() => setNavOpen(true)}
-      />
+      <FloatingActions onMenuClick={() => setNavOpen(true)} />
 
       <Header />
       {children}
